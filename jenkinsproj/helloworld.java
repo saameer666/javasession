b2d0e6cc9af74ed5bdc013db47ce54f15262d303
saameer666 <105152173+saameer666@users.noreply.github.com>
2026-07-24 07:45:30 +0530

    Update welcome message in helloworld.java

--- jenkinsproj/helloworld.java
+++ jenkinsproj/helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("welcome to java session mr.sameer shaik");
+        System.out.println("welcome to java session mr.sameer shaik-testing poll scm now");
     }
 }
